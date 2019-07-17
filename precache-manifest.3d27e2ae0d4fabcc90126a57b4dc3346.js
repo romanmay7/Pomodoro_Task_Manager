38db721f14cc06a95ed4620fd7e6c47d801f27c2
romanmayerzon@gmail.com <romanmayerzon@gmail.com>
2019-07-17 06:42:42 +0300

    Updates

---
+++ precache-manifest.3d27e2ae0d4fabcc90126a57b4dc3346.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464059c708f31b347617a515470aa1a4",
+    "url": "/Pomodoro_Task_Manager/index.html"
+  },
+  {
+    "revision": "42d10eb883ee49ee9544",
+    "url": "/Pomodoro_Task_Manager/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "bd8291a98e8010c6fd99",
+    "url": "/Pomodoro_Task_Manager/static/css/main.056e31e3.chunk.css"
+  },
+  {
+    "revision": "42d10eb883ee49ee9544",
+    "url": "/Pomodoro_Task_Manager/static/js/2.6b2e1192.chunk.js"
+  },
+  {
+    "revision": "bd8291a98e8010c6fd99",
+    "url": "/Pomodoro_Task_Manager/static/js/main.abe278d6.chunk.js"
+  },
+  {
+    "revision": "6d2b46b4cd37c74c3a1a",
+    "url": "/Pomodoro_Task_Manager/static/js/runtime~main.16219b5d.js"
+  },
+  {
+    "revision": "d53b05dabc643ceecbf5475096aa836b",
+    "url": "/Pomodoro_Task_Manager/static/media/logo.d53b05da.svg"
+  }
+]);
